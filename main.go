bb85c5d01f125d006cbbd51cdf56e221590496d4
jantb <jan@boee.org>
2016-10-17 14:39:42 +0200

    Initial

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 	"github.com/blevesearch/bleve/mapping"
 	"github.com/blevesearch/bleve/analysis/lang/en"
 	"github.com/blevesearch/bleve/analysis/analyzer/keyword"
+	"github.com/blevesearch/bleve/document"
 )
 
 var username = ""
 var password = ""
+var me = username
 
 // Jira server without http://
 var jiraServer = ""
@@ -29,7 +31,7 @@ func main() {
 		panic(err)
 	}
 
-	list, _, _ := jiraClient.Issue.Search("", &jira.SearchOptions{StartAt:0, MaxResults:1})
+	list, _, _ := jiraClient.Issue.Search("filter=16228", &jira.SearchOptions{StartAt:0, MaxResults:100})
 
 	m, err := buildIndexMapping()
 	if err != nil {
@@ -41,23 +43,24 @@ func main() {
 	}
 
 	for _, l := range list {
-		fmt.Println(l.Fields.Description)
-		fmt.Println(l.Fields.Summary)
 		err = index.Index(l.ID, l)
 		if err != nil {
 			log.Panic(err)
 		}
 	}
 
-	query := bleve.NewMatchQuery("begge")
+	query := bleve.NewMatchAllQuery()
 	search := bleve.NewSearchRequest(query)
+	search.Size = 100
+	search.SortBy([]string{"-_id"})
 	searchResults, err := index.Search(search)
 	if err != nil {
 		log.Panic(err)
 	}
-	b, err := index.Document(searchResults.Hits[0].ID)
-	fmt.Println(string(b.Fields[2].Value()))
-	//index.
+	for _, h := range searchResults.Hits {
+		b, _ := index.Document(h.ID)
+		printIssue(b)
+	}
 }
 
 func buildIndexMapping() (mapping.IndexMapping, error) {
@@ -70,12 +73,16 @@ func buildIndexMapping() (mapping.IndexMapping, error) {
 	keywordFieldMapping := bleve.NewTextFieldMapping()
 	keywordFieldMapping.Analyzer = keyword.Name
 
+	// a generic reusable mapping for keyword text
+	dateFieldMapping := bleve.NewDateTimeFieldMapping()
+
+
 	mapping := bleve.NewDocumentMapping()
 
-	mapping.AddFieldMappingsAt("Summary", textFieldMapping)
+	mapping.AddFieldMappingsAt("fields.summary", textFieldMapping)
+	mapping.AddFieldMappingsAt("fields.updated", dateFieldMapping)
 
-	mapping.AddFieldMappingsAt("Description", textFieldMapping)
-
+	mapping.AddFieldMappingsAt("fields.description", textFieldMapping)
 
 	indexMapping := bleve.NewIndexMapping()
 	indexMapping.AddDocumentMapping("issue", mapping)
@@ -84,4 +91,59 @@ func buildIndexMapping() (mapping.IndexMapping, error) {
 	indexMapping.DefaultAnalyzer = "en"
 
 	return indexMapping, nil
+}
+
+func printIssue(issue *document.Document) {
+	var priorityValue = ""
+	var creator = ""
+	var assignee = ""
+	var key = ""
+	var updated = ""
+	var summary = ""
+	var status = "status"
+	var fix = ""
+	for _, value := range issue.Fields {
+		if value.Name() == "fields.priority.name" {
+			priorityValue = string(value.Value())
+		}
+		if value.Name() == "fields.Creator.name" {
+			creator = string(value.Value())
+		}
+		if value.Name() == "fields.assignee.name" {
+			assignee = string(value.Value())
+		}
+		if value.Name() == "key" {
+			key = string(value.Value())
+		}
+		if value.Name() == "fields.updated" {
+			updated = string(value.Value())
+		}
+		if value.Name() == "fields.summary" {
+			summary = string(value.Value())
+		}
+		if value.Name() == "fields.status.name" {
+			status = string(value.Value())
+		}
+		if value.Name() == "fields.fixVersions.name" {
+			fix = string(value.Value())
+		}
+	}
+	var priority = ""
+	if priorityValue == "Minor" {
+		priority = fmt.Sprintf("\033[0;32m%-10s\033[m", priorityValue)
+	} else if priorityValue == "Major" {
+		priority = fmt.Sprintf("\033[0;31m%-10s\033[m", priorityValue)
+	} else if priorityValue == "Blocker" {
+		priority = fmt.Sprintf("\033[0;30;41m%-10s\033[m", priorityValue)
+	} else {
+		priority = fmt.Sprintf("%s", priorityValue)
+	}
+	if assignee == me {
+		assignee = fmt.Sprintf("\033[1;31m%-10s\033[m", me)
+	}
+	if creator == me {
+		assignee = fmt.Sprintf("\033[1;31m-10%s\033[m", me)
+	}
+	fmt.Printf("%-15s %-15s %-10s %-10s %-10s %-20s %-20s %s\n",
+		key, updated[:len("2006-01-02T15:04:05")], priority, assignee, creator, fix, status, summary)
 }
